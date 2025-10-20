9d59cd1474f54c162a96a7818e849ccf8b57c647
dynii <dyniiyeyo@naver.com>
2025-10-21 08:53:52 +0900

    Directory Renewal

--- html-css-js/learn-javascript/DOM/js/ex3.js
+++
@@ -1,4 +0,0 @@
-window.onload = function () {
-  document.querySelector('h1').style.backgroundColor = 'green';
-  document.querySelector('h2').style.backgroundColor = 'pink';
-};
--- html-css-js/learn-javascript/DOM/js/ex7.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-  const header = this.document.getElementById('header');
-
-  header.style.color = 'red';
-  header.style.border = '3px solid gray';
-};
